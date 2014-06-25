702ce233eb9b4cb410c70336c534789aa1c29b32
Nyholm <tobias.nyholm@gmail.com>
2014-06-25 15:56:45 +0200

    Removed the command

--- Command/PurgeAsseticCacheCommand.php
+++ Command/PurgeAsseticCacheCommand.php
@@ -24,8 +24,8 @@ class PurgeAsseticCacheCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $cloudFlare=$this->getContainer()->get('happyr.clourflare.service.cloudflare');
-        $conf=$this->getContainer()->getParameter('templating.assets_base_urls');
+        $conf=$this->getContainer()->getParameter('framework.templating.assets_base_urls');
 
-        var_dump($conf);
+        var_dump($this->getContainer()->p);
     }
 }
